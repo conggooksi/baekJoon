7bedb56ad6caaa9fd465f66b1a7c8a8bf6e3d920
kappa <kimgeonkuk@gmail.com>
2022-08-02 21:59:17 +0900

    [temp]test4673

--- main.go
+++ main.go
@@ -3,12 +3,42 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(d(i))
+	}
 
-	fmt.Println(d(39))
+
+	var a[] int
+	var b[] int
+	for i := 1; i <= 10; i++ {
+		a = append(a, i)
+	}
+
+	for i := 0; i <= 10; i++ {
+		for j := 1; j <= 10; j++ {
+			if i == d(j) {
+				b = append(a[:i], a[i+1:]...)
+			}
+		}
+
+	}
+	fmt.Println(b)
+
 }
 
 func d(n int) int {
-	num := n + n/10 + n%10
+	var num int	
+	num = n
+	for true{
+		remain := n%10
+	
+		num = num + remain
+		n = n/10
+
+		if n == 0 {
+			break
+		}
+	}
 
 	return num
 }
