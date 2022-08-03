4a9917402b0a8b75168ace4cc3023f1ac9d41707
김건국 <kuki@gimgeongug-ui-MacBookAir.local>
2022-08-03 17:51:46 +0900

    [oneMore]test4673

--- main.go
+++ main.go
@@ -3,45 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
-		fmt.Println(d(i))
+	selfNums := selfNum(10000)
+
+	for i := 1; i < len(selfNums); i++ {
+		if selfNums[i] == true {
+			fmt.Println(i)
+		}
 	}
-
-
-	var a = []int64{1,2,3,4,5}
-	var b []int64
-
-	b = append(a[:2], a[2+1:]...),
-
-	// for i := 1; i <= 10; i++ {
-	// 	a = append(a, i)
-	// }
-
-	// for i := 0; i <= len(a); i++ {
-	// 	for j := 1; j <= 10; j++ {
-	// 		if i == d(j) {
-	// 			b = append(a[:i], a[i+1:]...)
-	// 		}
-	// 	}
-
-	// }
-	fmt.Printf("%v\n", b)
-
 }
 
-func d(n int) int {
-	var num int	
-	num = n
-	for true{
-		remain := n%10
-	
-		num = num + remain
-		n = n/10
+func selfNum(n int) (chkNum map[int]bool) {
 
-		if n == 0 {
-			break
+	chkNum = make(map[int]bool, n+1)
+	for i := 0; i < n+1; i++ {
+		chkNum[i] = true
+	}
+
+	for i := 0; i < n+1; i++ {
+		var sum = i
+		var num = i
+		for j := num; j != 0; j /= 10 {
+			sum += j % 10
+		}
+		if sum <= n {
+			chkNum[sum] = false
 		}
 	}
 
-	return num
+	return chkNum
 }
