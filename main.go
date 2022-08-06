855c18eae71625cd3e714a341a9fd4e469ef5245
kappa <kimgeonkuk@gmail.com>
2022-08-06 16:58:44 +0900

    [TEMP]

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"os"
-	"strings"
-)
-
 func main() {
-	br := bufio.NewReader(os.Stdin)
-	str, _ := br.ReadString('\n')
-
-	eraseLine := strings.Fields(str)
-
-	a := []byte(eraseLine)
-
-	// splitedText := strings.Split(eraseLine[0], "")
 
 }
