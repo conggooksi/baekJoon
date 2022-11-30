c19060b4d99d977bf9f43994d0fe9afbb585cb6f
kuki <kimgeonkuk@gmail.com>
2022-11-30 09:55:03 +0900

    [ADD]test2587

--- test2587/test2587.go
+++ test2587/test2587.go
@@ -1,1 +1,32 @@
 package test2587
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func Test2587() {
+
+	var numbers []int
+	var n, sum int
+	defer writer.Flush()
+
+	for i := 0; i < 5; i++ {
+		fmt.Fscanln(reader, &n)
+
+		numbers = append(numbers, n)
+	}
+
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+
+	sort.Ints(numbers)
+
+	fmt.Fprintln(writer, sum/5, numbers[2])
+}
