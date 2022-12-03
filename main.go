139b8ac0c8ec6cdd1ae7df8922ef67569414eaaa
kappa <kimgeonkuk@gmail.com>
2022-12-03 09:00:21 +0900

    [ADD]test1427

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n int
+
+	fmt.Fscanln(reader, &n)
+
+	fmt.Fprintln(writer, n)
 }
